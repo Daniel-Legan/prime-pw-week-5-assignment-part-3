72d357651d3e4ed3381b6379024dd5b77a7a1ab3
Daniel Legan <legan.daniel@gmail.com>
2022-09-15 16:24:44 -0500

    added showCollection function and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,8 +3,16 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(x, y, z) {
-    collection.push({"title": x, "artist": y, "yearPublished": z});
+    collection.push({'title': x, 'artist': y, 'yearPublished': z});
     return console.log('the album added to collection:', collection[collection.length-1]);
+}
+
+function showCollection(album) {
+    console.log('the number of albums in this collection is:', collection.length);
+    for(let object of album) {
+        //console.log('TITLE by ARTIST, publushed in YEAR');
+        console.log(object.title, 'by', object.artist, 'published in,', object.yearPublished);
+    }
 }
 
 addToCollection('hello', 'beyonce', '2022');
@@ -15,4 +23,6 @@ addToCollection('grape', 'apple', '2009');
 addToCollection('i love you', 'pear', '1967');
 
 console.log('the whole collection of albums:', collection);
-console.log(collection[1]);
+
+showCollection(collection);
+console.log(collection[0].title)
